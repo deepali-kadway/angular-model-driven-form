50f69fb28bd7005716c91f1a3e4a2707a95b8a71
Deepali Kadway <dkadway@gmail.com>
2025-08-19 22:51:47 -0600

    built-in validation

--- src/app/components/form-component/form-component.ts
+++ src/app/components/form-component/form-component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-component',
@@ -14,13 +14,24 @@ loginForm!: FormGroup;
 constructor(private formBuilderInstance: FormBuilder){
   // create a form group with two form controls: email, password, subscribe
  this.loginForm = this.formBuilderInstance.group({
-    email: '',
-    password: '',
+    email: ['', [Validators.required, Validators.email, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
     subscribe: false,
   })
 }
 
+// getter for loginForm email control. This will allow us to access the email control in our html file (in the pre tag)
+get email(){
+  return this.loginForm.get('email');
+}
+
+get password() {
+  return this.loginForm.get('password');
+}
+
 onSubmit(){
-  console.log('Form Submitted:', this.loginForm.value);
+  if (this.loginForm.valid){
+console.log('Form Submitted:', this.loginForm.value);
+  }
 }
 }
